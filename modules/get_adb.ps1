b390324659b96545f5f0f8549e2585af6f4db100
botato <botatostuff@gmail.com>
2022-02-03 11:35:27 -0500

    add multiple locations, and if not found download

--- modules/get_adb.ps1
+++ modules/get_adb.ps1
@@ -1,4 +1,38 @@
-$adb = "C:\platform-tools\adb.exe";
+$commonAdbLocations = @(
+  "$Env:LOCALAPPDATA\Android\sdk\platform-tools\adb.exe",
+  "C:\platform-tools\adb.exe",
+  "C:\adb\adb.exe"
+)
+
+Function Test-CommandExists {
+  Param ([string] $command)
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = "stop"
+  try {
+    if (Get-Command $command) {
+      Return $true
+    }
+  } Catch {
+    Return $false
+  } Finally {
+    $ErrorActionPreference = $oldPreference
+  }
+}
+
+ForEach ($adbPath in $commonAdbLocations) {
+  if (Test-Path -Path $adb) {
+    $adb = $adbPath
+    break
+  }
+}
+
+if (!Get-Variable -Name adb -ErrorAction SilentlyContinue) {
+  Write-Host "adb not found, downloading..."
+  $adb_download_url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
+  Invoke-WebRequest -Uri $adb_download_url -OutFile "$Env:TEMP\platform-tools.zip"
+  Expand-Archive -LiteralPath "$Env:TEMP\platform-tools.zip" -DestinationPath "C:"
+  $adb = "C:\platform-tools\adb.exe"
+}
 
 if ((& $adb get-state) -Contains "error") {
   Write-Host "Device not found. Connect your device and try again." -ForegroundColor Red
