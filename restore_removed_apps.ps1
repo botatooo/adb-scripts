3497b9280f019c865c0fa57a388f9733c08246fe
botato <botatostuff@gmail.com>
2022-02-13 13:04:39 -0500

    Remove get_removed_apps.ps1 dependency

--- restore_removed_apps.ps1
+++ restore_removed_apps.ps1
@@ -2,8 +2,19 @@ Get-Content -Path .\modules\get_adb.ps1 | Invoke-Expression
 
 Write-Host "Removed files: " -ForegroundColor Green
 
-& .\get_removed_apps.ps1 -NoVerbose
+$Params = @{
+  # Only the packages that are installed
+  ReferenceObject = (& $adb shell pm list packages)
+  # All the packages including the ones that are uninstalled
+  DifferenceObject = (& $adb shell pm list packages -u)
+}
 
+# Get the uninstalled packages
+$RemovedPackages = Compare-Object @Params -PassThru
+# Clean it up
+$RemovedPackages = $RemovedPackages -replace 'package:',''
+# Sort it
+$RemovedPackages = $RemovedPackages | Sort-Object
 
 Write-Host "Reinstalling apps..." -ForegroundColor Green
 
