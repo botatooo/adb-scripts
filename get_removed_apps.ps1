5a1655de535e07c0cbf2401df3cd4c9227af18b1
botato <botatostuff@gmail.com>
2022-02-18 11:54:06 -0500

    Use new modules, Fix writing to host

--- get_removed_apps.ps1
+++ get_removed_apps.ps1
@@ -1,4 +1,4 @@
-Get-Content -Path .\modules\get_adb.ps1 | Invoke-Expression
+Import-Module -Name './modules/get_adb.psm1'
 
 $Params = @{
   # Only the packages that are installed
@@ -16,13 +16,9 @@ $RemovedPackages = $RemovedPackages | Sort-Object
 
 ## Next step: use package IDs to get the app names
 # Add aapt
-& $adb push ./bin/aapt-arm-pie /data/local/tmp
-& $adb shell chmod 0755 /data/local/tmp/aapt-arm-pie
+Import-Module -Name "./modules/add_aapt.psm1"
 
-Write-Host "AAPT has been added to the temporary directory of your device." -ForegroundColor Green
-
-$RemovedApps = @()
-
+Write-Host "Uninstalled apps:" -ForegroundColor Red
 ForEach ($PackageID in $RemovedPackages) {
   # Default output
   $path = (adb shell pm list packages -u -f "$PackageID") | Select-Object -First 1
@@ -30,21 +26,10 @@ ForEach ($PackageID in $RemovedPackages) {
   $path = $path -replace "package:","" -replace "=$PackageID",""
   # Use AAPT to get the app name
   $AppName = (adb shell /data/local/tmp/aapt-arm-pie dump badging "$path")
-  # Clean it up
   $AppName = $AppName | Select-String -Pattern "application-label:"
+  # Clean it up
   $AppName = $AppName -replace "application-label:'","" -replace "'",""
   $AppName = $AppName -replace "\\",""
   # Print it out
-  $RemovedApps += @($AppName, $PackageID)
+  Write-Host "$AppName ($PackageID)" -ForegroundColor Yellow
 }
-
-Write-Host $RemovedApps -ForegroundColor Yellow
-
-# Print it into the console (with a new line seperating them)
-ForEach ($App in $RemovedApps) {
-  $AppName = $App[0]
-  $Package = $App[1]
-  Write-Host "$AppName"," ($Package)" -ForegroundColor Red,Yellow
-}
-
-#Write-Host ($RemovedApps -join [Environment]::NewLine) -ForegroundColor Red
